f0866b17dafaded617d0386c4842fa792c69e32c
Hendri Pretorius <pretorh@gmail.com>
2017-09-24 22:23:56 +0200

    basic reading.writing

--- index.js
+++ index.js
@@ -7,8 +7,37 @@ r.connect()
     console.log('connected!');
     connection = c;
   })
+  .then(createTable)
+  .then(write)
+  .then(read)
+  .catch(console.error)
   .then(() => {
     console.log('closing connection');
     return connection.close();
   })
   .catch(console.error)
+
+function createTable() {
+  console.log('=== create table ===');
+  return r.tableDrop('test').run(connection)
+    .catch(/* ignore */)
+    .then(() => {
+      return r.tableCreate('test').run(connection)
+        .then(console.log)
+    })
+}
+
+function write() {
+  console.log('=== write ===');
+  return r.table('test').insert({ a: 1, b: 2 }).run(connection)
+    .then(console.log)
+    .then(() => r.table('test').insert([{ a: 10, b: 20 }, { a: 15, b: 30 }]).run(connection))
+    .then(console.log)
+}
+
+function read() {
+  console.log('=== read ===');
+  return r.table('test').run(connection)
+    .then(e => e.toArray())   // call toArray on cursor result to get all the data
+    .then(console.log)
+}
